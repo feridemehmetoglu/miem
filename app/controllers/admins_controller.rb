705b30868208b12296a81298afef2c32395636a1
feridemehmetoglu <feride@code2.co>
2016-04-14 16:37:06 +0300

    home page bootstrap

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,6 @@
+class AdminsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'admins/index'
+
   devise_for :users
 
   root 'home#index'
