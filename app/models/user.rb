c89c34f7a66f07311f2ee59799a8d9587b4366b9
Burak Dogan <burak@code2.co>
2016-04-16 12:05:18 +0300

    user modelin ilişkileri düzeltildi

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   has_many :exams
-<<<<<<< HEAD
+  
   has_many :education_records
   has_many :educations, through: :education_records      
-=======
+  
   has_many :education_pre_records
-<<<<<<< HEAD
-  has_many :education, through: :education_pre_records   
+  
   has_one :user_profile   
-=======
-  has_many :educations, through: :education_pre_records      
->>>>>>> 05eb04e3060e71809b714b08e7b2179ac7f29bdf
->>>>>>> 15c0a90d21f3f543e857f53c77afb9edd7ad632e
+
+
 end
