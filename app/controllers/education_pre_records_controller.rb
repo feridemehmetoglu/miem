57a08eb5c9e8f931c2373815ee1d07b839863ff1
feridemehmetoglu <feride@code2.co>
2016-04-16 12:07:46 +0300

    prerecord

--- app/controllers/education_pre_records_controller.rb
+++ app/controllers/education_pre_records_controller.rb
@@ -1,11 +1,26 @@
 class EducationPreRecordsController < ApplicationController
 
 	def index
-		@all_education = Education.all
-		@pre_education = Education.where("finish_date <?", Date.yesterday) #finish_date dün ve daha önce olan tarihdeki eğitimleri getirir
+	
+		@pre_education = Education.where("finish_date <=?", Date.yesterday) #finish_date dün ve daha önce olan tarihdeki eğitimleri getirir
 	end
 
+	def new
+		@pre_record = EducationPreRecord.new
+	end
 
+	def show
 
+   education = Education.find(params[:id])
+   EducationPreRecord.create(user_id: current_user.id , education_id: education.id  ) 
+   redirect_to root_path  
+		
+	end
+
+   private
+   def pre_record
+   	params.require(education_pre_record).permit(:user_id,:education_id)
+   	
+   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,8 @@ class User < ActiveRecord::Base
   
   has_many :exams
   has_many :education_pre_records
-<<<<<<< HEAD
+
   has_many :education, through: :education_pre_records   
   has_one :user_profile   
-=======
-  has_many :educations, through: :education_pre_records      
->>>>>>> 05eb04e3060e71809b714b08e7b2179ac7f29bdf
+
 end
