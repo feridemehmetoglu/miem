4ce7c3e5ffa200fc24b381e9158c3aa836398136
Burak Dogan <burak@code2.co>
2016-04-20 09:45:22 +0300

    will_paginate yapıldı.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	before_action :authenticate_user!
   def index
-    @education=Education.where("start_date >= :start_date", start_date:Date.today)
+    @education=Education.where("start_date >= :start_date", start_date:Date.today).page(params[:page]).per_page(4)
   end
 end
